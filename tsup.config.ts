d5831a887c0be640eb85c951586948d2bc3e7e1b
glitch452 <894128+glitch452@users.noreply.github.com>
2025-02-18 09:04:35 -0800

    style: Cleanup tsup config file

--- tsup.config.ts
+++ tsup.config.ts
@@ -30,7 +30,6 @@ const banner = ((_context) => {
 const tsupConfig = defineConfig({
   banner,
   bundle: true,
-  // legacyOutput: true,
   clean: true,
   dts: true,
   entry: ['src/**/index.js'],
@@ -38,31 +37,8 @@ const tsupConfig = defineConfig({
   minify: true,
   outDir: 'dist',
   skipNodeModulesBundle: true,
-  // splitting: false,
   target: 'es2022',
   watch: false,
-  // plugins: [
-  //   {
-  //     // https://github.com/egoist/tsup/issues/953#issuecomment-2294998890
-  //     // ensuring that all local requires/imports in `.cjs` files import from `.cjs` files.
-  //     // require('./path') → require('./path.cjs') in `.cjs` files
-  //     // require('../path') → require('../path.cjs') in `.cjs` files
-  //     // from './path' → from './path.cjs' in `.cjs` files
-  //     // from '../path' → from '../path.cjs' in `.cjs` files
-  //     name: 'fix-cjs-imports',
-  //     renderChunk(code) {
-  //       if (this.format === 'cjs') {
-  //         const regexCjs = /require\((?<quote>['"])(?<import>\.[^'"]+)\.js['"]\)/g;
-  //         const regexEsm = /from(?<space>[\s]*)(?<quote>['"])(?<import>\.[^'"]+)\.js['"]/g;
-  //         return {
-  //           code: code
-  //             .replace(regexCjs, 'require($<quote>$<import>.cjs$<quote>)')
-  //             .replace(regexEsm, 'from$<space>$<quote>$<import>.cjs$<quote>'),
-  //         };
-  //       }
-  //     },
-  //   },
-  // ],
 });
 
 export default tsupConfig;
