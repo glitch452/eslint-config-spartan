0806b00283c1cdbd2416b9bf22d9d6b5ff7a0548
glitch452 <894128+glitch452@users.noreply.github.com>
2025-02-27 18:35:47 -0800

    chore: Move lint-staged config to its own file

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+export default {
+  '*.{md,mdx,mjs,cjs,js,jsx,cjsx,mjsx,mts,cts,ts,tsx,ctsx,mtsx}': [
+    'eslint --cache --report-unused-disable-directives --fix',
+    'prettier --ignore-unknown --write',
+  ],
+  '*.{css,html,json,scss,yaml,yml}': 'prettier --ignore-unknown --write',
+};
