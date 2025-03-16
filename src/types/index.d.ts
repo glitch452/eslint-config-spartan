d7b3f905cc3dc6d2e3f8dd6320101d79117cd440
glitch452 <894128+glitch452@users.noreply.github.com>
2025-03-15 22:33:37 -0700

    refactor: Cleanup unneecssary type overrides that have types defined natively or in DefinitelyTyped

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -25,22 +25,7 @@ declare module '@next/eslint-plugin-next' {
   export default module;
 }
 
-declare module 'eslint-plugin-react' {
-  const module: EsLintPlugin;
-  export default module;
-}
-
-declare module 'eslint-plugin-unused-imports' {
-  const module: EsLintPlugin;
-  export default module;
-}
-
 declare module 'eslint-plugin-typescript-enum' {
-  const module: EsLintPlugin;
-  export default module;
-}
-
-declare module 'eslint-plugin-jsx-a11y' {
   const module: EsLintPlugin;
   export default module;
 }
@@ -65,42 +50,12 @@ declare module 'eslint-plugin-googleappsscript' {
   export default module;
 }
 
-declare module 'eslint-plugin-i18next' {
-  const module: EsLintPlugin;
-  export default module;
-}
-
 declare module 'eslint-plugin-i18n-json' {
-  const module: EsLintPlugin;
-  export default module;
-}
-
-declare module 'eslint-plugin-testing-library' {
   const module: EsLintPlugin;
   export default module;
 }
 
 declare module 'eslint-plugin-promise' {
   const module: EsLintPlugin;
-  export default module;
-}
-
-declare module 'eslint-plugin-jest' {
-  const module: EsLintPlugin;
-  export default module;
-}
-
-declare module 'eslint-plugin-jest-dom' {
-  const module: EsLintPlugin;
-  export default module;
-}
-
-declare module 'remark-lint-no-dead-urls' {
-  const module: RemarkPlugin;
-  export default module;
-}
-
-declare module 'remark-lint-check-toc' {
-  const module: RemarkPlugin;
   export default module;
 }
